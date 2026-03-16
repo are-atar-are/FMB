da5b833bbeea3569a30a05a21d33dfaf9272e98b
ARE ATAR <areatar@AREs-Mac-mini.local>
2026-03-16 11:00:28 +0200

    feat: implement Calendar and VehicleSelection components with filters

--- src/features/booking/screens/BookingScreen.tsx
+++ src/features/booking/screens/BookingScreen.tsx
@@ -4,23 +4,29 @@ import {
   Text,
   StyleSheet,
   ScrollView,
-  TouchableOpacity,
 } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
-import { AppDispatch, RootState } from '../../store/store';
+import { AppDispatch } from '../../store/store';
 import {
   setSelectedDate,
   setSelectedVehicle,
   setShowCarInfoModal,
   setVehicles,
 } from '../redux/bookingSlice';
+import {
+  selectVehicles,
+  selectSelectedDate,
+  selectSelectedVehicle,
+} from '../redux/bookingSelectors';
 import { Vehicle } from '../types';
+import Calendar from '../components/Calendar';
+import VehicleSelection from '../components/VehicleSelection';
 
 const BookingScreen: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { vehicles, selectedDate, selectedVehicle } = useSelector(
-    (state: RootState) => state.booking
-  );
+  const vehicles = useSelector(selectVehicles);
+  const selectedDate = useSelector(selectSelectedDate);
+  const selectedVehicle = useSelector(selectSelectedVehicle);
 
   useEffect(() => {
     // Load vehicles (in real app, this would come from Firestore)
@@ -57,33 +63,63 @@ const BookingScreen: React.FC = () => {
         pricePerDay: 'R450',
         vehicleImage: 'toyota-hilux',
       },
+      {
+        id: '3',
+        name: 'VW Golf',
+        type: 'Hatch',
+        available: true,
+        registration: 'DEF456GP',
+        fuelType: 'Petrol',
+        power: '110 KW',
+        topSpeed: '200 km/h',
+        location: 'Durban',
+        bookingTimeSlots: ['8 AM', '9 AM', '10 AM'],
+        driverName: 'Mike Johnson',
+        driverRole: 'Driver',
+        pricePerDay: 'R350',
+        vehicleImage: 'vw-golf',
+      },
     ];
     dispatch(setVehicles(mockVehicles));
   }, [dispatch]);
+
+  const handleSelectDate = (date: string) => {
+    dispatch(setSelectedDate(date));
+  };
 
   const handleSelectVehicle = (vehicle: Vehicle) => {
     dispatch(setSelectedVehicle(vehicle));
     dispatch(setShowCarInfoModal(true));
   };
 
+  const handleSelectDays = (vehicle: Vehicle) => {
+    // TODO: Open days bottom sheet
+    console.log('Select days for:', vehicle.name);
+  };
+
+  const handleSelectTime = (vehicle: Vehicle) => {
+    // TODO: Open time bottom sheet
+    console.log('Select time for:', vehicle.name);
+  };
+
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.header}>Book a Vehicle</Text>
       
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Available Vehicles</Text>
-        {vehicles.map((vehicle) => (
-          <TouchableOpacity
-            key={vehicle.id}
-            style={styles.vehicleCard}
-            onPress={() => handleSelectVehicle(vehicle)}
-          >
-            <Text style={styles.vehicleName}>{vehicle.name}</Text>
-            <Text style={styles.vehicleType}>{vehicle.type}</Text>
-            <Text style={styles.vehiclePrice}>{vehicle.pricePerDay}/day</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
+      {/* Calendar */}
+      <Calendar
+        selectedDate={selectedDate}
+        onSelectDate={handleSelectDate}
+      />
+      
+      {/* Vehicle Selection */}
+      <VehicleSelection
+        vehicles={vehicles}
+        selectedVehicle={selectedVehicle}
+        onSelectVehicle={handleSelectVehicle}
+        onSelectDays={handleSelectDays}
+        onSelectTime={handleSelectTime}
+      />
     </ScrollView>
   );
 };
@@ -94,40 +130,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#f5f5f5',
   },
   header: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
     padding: 16,
     backgroundColor: 'white',
-  },
-  section: {
-    padding: 16,
-  },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginBottom: 12,
-  },
-  vehicleCard: {
-    backgroundColor: 'white',
-    padding: 16,
-    borderRadius: 8,
-    marginBottom: 12,
-    elevation: 2,
-  },
-  vehicleName: {
-    fontSize: 18,
-    fontWeight: '600',
-  },
-  vehicleType: {
-    fontSize: 14,
-    color: '#666',
-    marginTop: 4,
-  },
-  vehiclePrice: {
-    fontSize: 16,
-    fontWeight: '600',
-    color: '#007AFF',
-    marginTop: 8,
   },
 });
 
